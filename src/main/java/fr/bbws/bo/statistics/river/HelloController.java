437463f25cde1c58a1a2b02f82c10f20c4fc69ce
Alexandre Lods <bebetterwithstats@gmail.com>
2018-10-21 11:44:35 +0200

    [DEV] Correction erreur compilation du code générer par Maven

--- src/main/java/fr/bbws/bo/statistics/river/HelloController.java
+++ src/main/java/fr/bbws/bo/statistics/river/HelloController.java
@@ -2,7 +2,7 @@ package fr.bbws.bo.statistics.river;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+// import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,18 +20,23 @@ public class HelloController {
 
     @Data
     static class Result {
-        private final int left;
-        private final int right;
-        private final long answer;
+        //private final int left; // TODO enlever ce code lorsque ca compile
+        //private final int right; // TODO enlever ce code lorsque ca compile
+        //private final long answer; // TODO enlever ce code lorsque ca compile
     }
 
     // SQL sample
     @RequestMapping("calc")
     Result calc(@RequestParam int left, @RequestParam int right) {
-        MapSqlParameterSource source = new MapSqlParameterSource()
+        /*
+    	MapSqlParameterSource source = new MapSqlParameterSource()
                 .addValue("left", left)
                 .addValue("right", right);
+        
+        
         return jdbcTemplate.queryForObject("SELECT :left + :right AS answer", source,
                 (rs, rowNum) -> new Result(left, right, rs.getLong("answer")));
+        */
+        return null; // TODO enlever ce code lorsque ca compile
     }
 }
